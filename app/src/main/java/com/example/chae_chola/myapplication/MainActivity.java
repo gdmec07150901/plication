3584547afb700694e1002db85aec3dcb5a17b83c
chae cho <1178671876@qq.com>
2016-12-14 11:11:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chae_chola/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chae_chola.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
